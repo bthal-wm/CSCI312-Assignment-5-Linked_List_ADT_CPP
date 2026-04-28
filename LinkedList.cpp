5b3d4a89e0809cd18ddaaef316861318f935b8ab
Brent Thal <bthal@wm.edu>
2026-04-28 13:35:54 -0400

    Add files via upload

---
+++ LinkedList.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstddef> //size_t
+#include "LinkedList.h"
+using namespace std;
+
+LinkedList::LinkedList() : head(nullptr), size(0){
+//TODO
+}
+
+LinkedList::~LinkedList(){
+//TODO
+}
+
+bool LinkedList::isEmpty() const{
+//TODO
+}
+
+size_t LinkedList::getSize() const {
+//TODO
+}
+
+void LinkedList::insertHead(int value){
+//TODO
+}
+
+void LinkedList::insertTail(int value){
+//TODO
+}
+
+void LinkedList::print() const{
+//TODO
+}
+
---
+++ LinkedList.h
@@ -0,0 +1,16 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <cstddef> //size_t
+#include <iostream>
+using namespace std;
+
+class LinkedList {
+private:
+// TODO
+
+public:
+// TODO
+};
+
+#endif // LINKEDLIST_H
